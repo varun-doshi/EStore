5582bb4c9b8a0893827e7e92147af8cbcc53a721
Varun Doshi <doshivarun202@gmail.com>
2023-04-23 12:48:25 +0530

    Fixed footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,26 @@
 import React from "react";
-import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
+import { AiFillLinkedin, AiOutlineTwitter } from "react-icons/ai";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <div className="footer-container">
       <p>2023 EStore All rights reserved</p>
       <p className="icons">
-        <AiFillInstagram />
-        <AiOutlineTwitter />
+        <div>
+          <Link href="https://www.linkedin.com/in/varunxdoshi/">
+            <a target="_blank">
+              <AiFillLinkedin />
+            </a>
+          </Link>
+        </div>
+        <div>
+          <Link href="https://twitter.com/Varunx10">
+            <a target="_blank">
+              <AiOutlineTwitter />
+            </a>
+          </Link>
+        </div>
       </p>
     </div>
   );
